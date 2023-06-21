eb513e85fafe1f043973e6b9c6c4f248931d9720
Herbert Beserra <Herbert.beserra@gmail.com>
2023-06-21 01:44:44 +0000

    Add: metodos a classe usuaro

--- go-backend/entity/user.go
+++ go-backend/entity/user.go
@@ -1,12 +1,15 @@
 package entity
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type UserType string
 
 const (
 	Default UserType = "user"
-	Admin            = "administrator"
+	Admin   UserType = "administrator"
 )
 
 type User struct {
@@ -18,4 +21,57 @@ type User struct {
 	document    Document
 	//address	AddressType
 	birthDate time.Time
+}
+
+func (u *User) GetName() string {
+	return u.fullName
+}
+
+func (u *User) GetDisplayName() string {
+
+	if len(u.displayName) <= 1 {
+		return u.fullName
+	}
+
+	return u.displayName
+}
+
+func (u *User) GetEmail() string {
+	return u.email
+}
+
+func (u *User) GetUserType() UserType {
+	return u.userType
+}
+
+func (u *User) GetDocument() Document {
+	return u.document
+}
+
+func (u *User) GetID() uint64 {
+	return u.id
+}
+
+func (u *User) GetBirthDate() time.Time {
+	return u.birthDate
+}
+
+func (u *User) GetAge() int {
+	return int(time.Since(u.birthDate).Hours() / 24 / 365)
+	//return int(time.Now().Sub(u.birthDate).Hours()/24/365) //u.birthDate
+}
+
+func (u *User) SetBrithDate(d time.Time) error {
+	if(int(time.Since(u.birthDate).Hours() / 24 / 365) <= 16){
+		return errors.New("user must be older than 16 years")
+	}
+
+	u.birthDate = d
+	return nil
+}
+
+func (u *User) SetName(name string) error {
+	
+
+	return nil
 }
